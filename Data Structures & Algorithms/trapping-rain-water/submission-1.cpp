591aa37ce549d46adefa0f3a82c60b8c5f2baa50
adityakumar003 <adiiv8609@gmail.com>
2026-07-13 17:02:15 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int l=0,r=n-1,lmax=0,rmax=0,res=0;
+        while(l<=r){
+            lmax=max(lmax,height[l]);
+            rmax=max(rmax,height[r]);
+            if(lmax<=rmax){
+                res+=lmax-height[l];
+                l++;
+            }else{
+                res+=rmax-height[r];
+                r--;
+            }
+        }
+        return res;
+    }
+};
